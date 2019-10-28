8deb2f6541e71ffe4331254f9bda4609b47dc732
Rodrigo Garcia <rodrigo.garcia@softplan.com.br>
2019-10-28 08:58:38 -0300

    Commit Secao 05

---
+++ Aulas/Secao05/Controllers/Teste01Controller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Secao05.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Teste01Controller : ControllerBase
+    {
+        // GET: api/Teste01
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Teste01/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Teste01
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Teste01/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
